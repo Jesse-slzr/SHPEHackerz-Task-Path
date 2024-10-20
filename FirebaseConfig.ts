a5451def82aa745af99a75eea24f6da078d1ca9b
EmilioLuchador <ebp0053@gmail.com>
2024-10-20 03:47:02 -0500

    created a login page and authentication to allow for users to register and login
    Connected the registration and login to firebase for user information via email
    
    login/registration page detects whether a user is registered or not.

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,22 +1,22 @@
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// // import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 // // TODO: Add SDKs for Firebase products that you want to use
 // // https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC_1sy5dOpisyJ2ViwC3IDZEfGVodW5Sbk",
-//   authDomain: "task-path.firebaseapp.com",
-//   projectId: "task-path",
-//   storageBucket: "task-path.appspot.com",
-//   messagingSenderId: "514036720601",
-//   appId: "1:514036720601:web:1756216c6b3c141b1cefc8",
-//   measurementId: "G-79VC5XRKCD"
-// };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ const firebaseConfig = {
+   apiKey: "AIzaSyC_1sy5dOpisyJ2ViwC3IDZEfGVodW5Sbk",
+   authDomain: "task-path.firebaseapp.com",
+   projectId: "task-path",
+   storageBucket: "task-path.appspot.com",
+   messagingSenderId: "514036720601",
+   appId: "1:514036720601:web:1756216c6b3c141b1cefc8",
+   measurementId: "G-79VC5XRKCD"
+ };
 
 // // Initialize Firebase
-// export const FIREBASE_APP = initializeApp(firebaseConfig);
-// export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
-// // const analytics = getAnalytics(FIREBASE_APP);
+ export const FIREBASE_APP = initializeApp(firebaseConfig);
+ export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+ const analytics = getAnalytics(FIREBASE_APP);
