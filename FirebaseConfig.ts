b67a1657f746cac748936e5601b97f0271ec093e
Emilio Pena <ebp0053@gmail.com>
2024-10-29 12:23:17 -0500

    updated window issue with login page - added another method to ensure if browser and environment is available for use and won't cause issues

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, isSupported } from "firebase/analytics";
 // // TODO: Add SDKs for Firebase products that you want to use
 // // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,4 +19,4 @@ import { getAnalytics } from "firebase/analytics";
 // // Initialize Firebase
  export const FIREBASE_APP = initializeApp(firebaseConfig);
  export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
- const analytics = getAnalytics(FIREBASE_APP);
+ const analytics = isSupported().then(yes => yes ? getAnalytics(FIREBASE_APP) : null);
